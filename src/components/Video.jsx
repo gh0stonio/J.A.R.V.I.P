0f9bd4726e365ac5d2b2023c645e60a7af75b4f4
ghostonio <antoine.leveque.pro@gmail.com>
2016-07-27 18:23:43 +0200

    Apply style on previous app & video components

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,14 +3,13 @@ import React, { Component } from 'react';
 export default class Video extends Component {
   render () {
     const style = {
-      width: 700,
-      height: 400
+      width: 600
     };
     const url = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4';
-    const poster = 'https://www.filmfestival.nl/FionaAssets/000001/00000136/scaled/13667.jpg';
+    const poster = 'http://images.2803.fr/uploads/2008/06/big-buck-bunny-2.jpg';
 
     return (
-      <video style={style} ref='videoDOM' src={url} poster={poster} controls></video>
+      <video style={style} ref='videoDOM' src={url} poster={poster}></video>
     );
   }
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Video from '../containers/Video';
-import FakeVideoTemplate from '../components/fakeVideoTemplate';
 import PlayerActions from '../actions';
 
 class App extends Component {
@@ -10,7 +9,51 @@ class App extends Component {
     const { actions } = this.props;
 
     return (
-      <FakeVideoTemplate />
+      <div className='jarvip container-fluid'>
+        <div className='row'>
+          <div className='col-xs-12'>
+            <Video actions={actions} />
+          </div>
+        </div>
+        <div className='control-bar'>
+          <div className='row'>
+            <div className='col-xs-12'>
+              <div className='progress-control'>
+                <div className='play-progress' style={{ width: '20%' }}></div>
+                <div className='load-progress' style={{ width: '40%' }}></div>
+                <div className='seek-handle' style={{ left: '20%' }}></div>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-xs-1'>
+              <div className='play-control control'>
+                <span className='fui-play'></span>
+              </div>
+            </div>
+            <div className='col-xs-3'>
+              <div className='control'>
+                <div className='time-control'>
+                  <span className='currentTime'>1:13:37</span>
+                  <span className='divider'> / </span>
+                  <span className='duration'>1:42:30</span>
+                </div>
+              </div>
+            </div>
+            <div className='col-xs-6'></div>
+            <div className='col-xs-1'>
+              <div className='volume-control control'>
+                <span className='fui-volume'></span>
+              </div>
+            </div>
+            <div className='col-xs-1'>
+              <div className='fullscreen-control control'>
+                <span className='fui-resize'></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
